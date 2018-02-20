2ca4565476e31425389beadeafae1ad0642c265a
Sarah Gilbert <sarahjgilbert1@yahoo.com>
2018-02-19 17:11:17 -0700

    updated with problems commented out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,16 +3,11 @@ $( document ).ready(function() {
     console.log( "ready!" );
 });
 
-var wins =0; //creates some variables to use throughout the script
-var losses =0;
-var sum =0; //this will be the sum of the gems added up to create your score so far
+var randomNumber = Math.floor((Math.random() * 120) + 19); 
+console.log("random" + randomNumber + "");
 
-var randomNumber = Math.floor((Math.random()*120)+19); //generates a random number between 19-120
- console.log("random" + randomNumber + "");
-//show a random target number between 19-120
-$("randomnumber").prepend("Random Number: " + randomNumber); //I don't know why this isn't showing up on the page. It's in the console.log...
-
-//var green = Math.floor(Math.random()*12)+1); // assigns a value between 1-12 to each crystal randomly (I don't know why green isn't working)
+$("#randomNumber").text("Random Number: " + randomNumber); //can't get the chosen random number to appear on page still pulling text from html
+//var green = Math.floor(Math.random()*12)+1);  I don't know why green isn't working
 var blue = Math.floor((Math.random()*12)+1);
 var red = Math.floor((Math.random()*12)+1);
 var yellow = Math.floor((Math.random()*12)+1);
@@ -22,42 +17,39 @@ console.log("yellow" + yellow + "");
 console.log("red" + red + "");
 console.log("blue" + blue + "");
 
-//add random number to sum (your score so far) when a crystal is clicked
-$(".green").click(function() {
-    sum = sum + green;
-})
-$(".blue").click(function() {
-    sum = sum + blue;
-})
-$(".red").click(function() {
-    sum = sum + red;
-})
-$(".yellow").click(function() {
-    sum = sum + yellow;
-})
-
-var text = function() {  //shows score as crystals are clicked
-    $("#wins").empty();
-    $("#wins").append(wins);
-    $("#losses").empty();
-    $("#losses").append(losses);
-    $(sum).empty();
-    $(sum).append(sum); //to get a running total
+var counter =0;
+var numberOptions = [blue, red, yellow,];  //add green when green works
+for (var i = 0; i < numberOptions.length; i++) {
+    var imageCrystal = $("<img>");
+    imageCrystal.addClass("crystal-image");
 }
 
-var outcome = function() {
-    if(sum === number) {
-        wins = wins + 1; //if your score = the random number, win
-        reset();
-    }
-    else(sum > number) 
-        losses = losses +1; //if your score goes over the random number, lose
-        reset();
-    }
-    var reset = function() {
-        sum = 0; //game restarts after win or loss
-        
-    }
+var imageCrystal = $("<img>"); //not sure about including all images in one line-
+imageCrystal.attr("src","assets/images/greencrystal.jpg", "assets/images/bluecartooncrystal.png", "assets/images/specialredcrystal.png", "yellowcartooncrystal.png");
+
+imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+$("<img>").on("click", function() { //something is wrong here-
+    console.log ("click"); //doesn't work
+
+    ///haven't looked at this part yet -from basic crystal game-
+var crystalValue = ($(this).attr("data-crystalvalue"));
+crystalValue = parseInt(crstalValue);
+counter =+ crystalValue;
+alert("new score" + counter);
+if (counter === randomNumber) {
+    alert("you win"); //change to update text on page
+
+}
+else if (counter>= randomNumber) {
+    alert("you lose"); //change to update text on page
+}
+})
+
+
+
+
+    
 
 
 

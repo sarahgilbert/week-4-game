dab870d2e04147a0845a68a9f65d9e9641a83b18
Sarah Gilbert <sarahjgilbert1@yahoo.com>
2018-02-24 08:02:37 -0700

    commit before changing functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,21 @@
 
 $( document ).ready(function() {
     console.log( "ready!" );
-});
 
+//choose a random target number
 var randomNumber = Math.floor((Math.random() * 120) + 19); 
 console.log("random" + randomNumber + "");
 
-$("#randomNumber").text("Random Number: " + randomNumber); //can't get the chosen random number to appear on page still pulling text from html
+//show the random target number on the page
+$("#randomNumber").text("Target Number: " + randomNumber); 
+
+// assigns a value between 1-12 to each crystal
 //var green = Math.floor(Math.random()*12)+1);  I don't know why green isn't working
 var blue = Math.floor((Math.random()*12)+1);
 var red = Math.floor((Math.random()*12)+1);
 var yellow = Math.floor((Math.random()*12)+1);
 
+//console logs the value assigned to each color crystal
 console.log("yellow" + yellow + "");
 //console.log("green" + green + "");
 console.log("red" + red + "");
@@ -21,22 +25,42 @@ var counter =0;
 var numberOptions = [blue, red, yellow,];  //add green when green works
 for (var i = 0; i < numberOptions.length; i++) {
     var imageCrystal = $("<img>");
-    imageCrystal.addClass("crystal-image");
+    imageCrystal.addClass("crystal-image"); 
 }
+$("#green").on("click", function() {
+    counter = counter + green; 
+    console.log("new green counter= " + counter);
+    if (counter === randomNumber) {
+        yay();
+    }
+    else if (counter > randomNumber) {
+        loser();
+    }
+})
 
-var imageCrystal = $("<img>"); //not sure about including all images in one line-
-imageCrystal.attr("src","assets/images/greencrystal.jpg", "assets/images/bluecartooncrystal.png", "assets/images/specialredcrystal.png", "yellowcartooncrystal.png");
+$("#blue").on("click", function() {
+    counter = counter + blue;
+    console.log("new blue counter" + counter);
+    if (counter ===randomNumber) {
+        yay();
+    if (counter> randomNumber) {
+        loser();
+    }
+    }
+})
+//var imageCrystal = $("<img>"); //not sure about including all images in one line-
+//imageCrystal.attr("src","assets/images/greencrystal.jpg", "assets/images/bluecartooncrystal.png", "assets/images/specialredcrystal.png", "yellowcartooncrystal.png");
 
-imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+//imageCrystal.data("data-crystalvalue", numberOptions[i]);
 
-$("<img>").on("click", function() { //something is wrong here-
-    console.log ("click"); //doesn't work
+$("img").on("click", function() { 
+    console.log ("click"); 
 
     ///haven't looked at this part yet -from basic crystal game-
 var crystalValue = ($(this).attr("data-crystalvalue"));
-crystalValue = parseInt(crstalValue);
+crystalValue = parseInt(crystalValue);
 counter =+ crystalValue;
-alert("new score" + counter);
+alert("new score" + counter); //alerting new score, but counter not working -change to Your total so far
 if (counter === randomNumber) {
     alert("you win"); //change to update text on page
 
@@ -45,7 +69,7 @@ else if (counter>= randomNumber) {
     alert("you lose"); //change to update text on page
 }
 })
-
+});
 
 
 
